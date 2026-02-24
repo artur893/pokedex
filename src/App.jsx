c492c498c022b3df1c272ded8935c606ab553115
Artur <arthal893@gmail.com>
2026-02-24 18:16:10 +0100

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Button } from "@material-tailwind/react";
-
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="solid">Solid</Button>
-      <Button variant="gradient">Gradient</Button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
