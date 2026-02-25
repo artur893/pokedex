dbe3803b00acb4ec567d9c9b2f6e9315258940c7
Artur <arthal893@gmail.com>
2026-02-25 18:24:08 +0100

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/shared/Navbar";
+import { Outlet } from "react-router";
 
 function App() {
   return (
     <>
       <div className="bg-gray-300 dark:bg-slate-800 text-gray-800 dark:text-slate-200 min-h-screen">
         <Navbar />
+        <Outlet />
       </div>
     </>
   );
