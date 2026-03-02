0a48e369041734258cb16234ce09fbc6ab983046
Artur <arthal893@gmail.com>
2026-03-02 20:59:21 +0100

    feat: add useLogin hook

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function useLogin() {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  async function login(identifier, password) {
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      const loginRes = await fetch(
+        `http://localhost:3001/users?login=${identifier}`,
+      );
+      const loginUser = await loginRes.json();
+      const emailRes = await fetch(
+        `http://localhost:3001/users?email=${identifier}`,
+      );
+      const emailUser = await emailRes.json();
+      const userRecord = [...loginUser, ...emailUser][0];
+
+      if (!userRecord) {
+        return {
+          ok: false,
+          message: "Nie znaleziono użytkownika",
+          variant: "error",
+        };
+      }
+      if (userRecord.password !== password) {
+        return {
+          ok: false,
+          message: "Hasło nieprawidłowe",
+          variant: "error",
+        };
+      }
+      setUser(userRecord);
+      return {
+        ok: true,
+        message: "Pomyślnie zalogowano",
+        variant: "success",
+      };
+    } catch (error) {
+      console.error(error);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  return { login, user, isLoading, isError };
+}
+
+export default useLogin;
