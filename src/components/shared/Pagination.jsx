81b19593cb1ec1ce1ed191442c9b351d0fd90009
Artur <arthal893@gmail.com>
2026-03-13 18:08:35 +0100

    feat: add pagination component

---
+++ src/components/shared/Pagination.jsx
@@ -0,0 +1,44 @@
+import { Button, IconButton } from "@material-tailwind/react";
+import { NavArrowRight, NavArrowLeft } from "iconoir-react";
+
+function Pagination({ totalItems, itemsOnPage, activePage, setPage }) {
+  const totalPages = Math.ceil(totalItems / itemsOnPage);
+  function populateButtons() {
+    const buttons = [];
+    for (let i = 1; i <= totalPages; i++) {
+      if (i >= activePage - 2 && i <= activePage + 2) {
+        buttons.push(
+          <IconButton
+            key={i}
+            isCircular
+            variant={activePage === i ? "solid" : "ghost"}
+            onClick={() => setPage(i)}
+          >
+            {i}
+          </IconButton>,
+        );
+      }
+    }
+    return buttons;
+  }
+
+  return (
+    <div className="flex items-center gap-1 self-center mt-4">
+      {activePage !== 1 && (
+        <Button isPill variant="ghost" onClick={() => setPage(activePage - 1)}>
+          <NavArrowLeft className="mr-1.5 h-4 w-4 stroke-2" />
+          Previous
+        </Button>
+      )}
+      {populateButtons()}
+      {activePage !== totalPages && (
+        <Button isPill variant="ghost" onClick={() => setPage(activePage + 1)}>
+          Next
+          <NavArrowRight className="ml-1.5 h-4 w-4 stroke-2" />
+        </Button>
+      )}
+    </div>
+  );
+}
+
+export default Pagination;
--- src/components/subpages/Home.jsx
+++ src/components/subpages/Home.jsx
@@ -1,12 +1,15 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PokemonContext } from "../../context/PokemonContext";
 import usePokemon from "../../hooks/usePokemon";
 import PokemonCard from "../shared/PokemonCard";
+import Pagination from "../shared/Pagination";
 
 function Home() {
   const { pokemonsContextData, setPokemonsContextData } =
     useContext(PokemonContext);
   const { pokemons, isLoading, isError } = usePokemon();
+  const [page, setPage] = useState(1);
+  const itemsOnPage = 15;
 
   useEffect(() => {
     setPokemonsContextData(pokemons);
@@ -24,11 +27,24 @@ function Home() {
     );
 
   return (
-    <div className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(250px,1fr))] place-items-center">
-      {pokemonsContextData.map((item) => (
-        <PokemonCard key={item.id} pokemon={item} />
-      ))}
-    </div>
+    <>
+      <div className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(250px,1fr))] place-items-center">
+        {pokemonsContextData
+          .slice(
+            itemsOnPage * (page - 1),
+            itemsOnPage * (page - 1) + itemsOnPage,
+          )
+          .map((item) => (
+            <PokemonCard key={item.id} pokemon={item} />
+          ))}
+      </div>
+      <Pagination
+        totalItems={pokemonsContextData.length}
+        itemsOnPage={itemsOnPage}
+        activePage={page}
+        setPage={setPage}
+      />
+    </>
   );
 }
 
