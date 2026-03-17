bb690198dc373af9f738e8b9deb689292013ef90
Artur <arthal893@gmail.com>
2026-03-17 21:15:15 +0100

    style: add vs between pokemon cards in arena component

--- src/components/subpages/Arena.jsx
+++ src/components/subpages/Arena.jsx
@@ -13,8 +13,14 @@ function Arena() {
       </p>
     );
   return (
-    <div className="flex">
+    <div className="flex flex-col md:flex-row gap-4 mt-8 place-self-center">
       <PokemonCard pokemon={arenaContextData[0]} />
+      <div class="flex items-center gap-2 self-center">
+        <span class="h-px w-8 bg-yellow-400"></span>
+        <span class="text-2xl font-bold text-yellow-400">VS</span>
+        <span class="h-px w-8 bg-yellow-400"></span>
+      </div>
+
       <PokemonCard pokemon={arenaContextData[1]} />
     </div>
   );
