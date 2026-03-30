5c4f003e5dc0c32d060f441f78b02af9ead3ab63
Artur <arthal893@gmail.com>
2026-03-30 22:43:39 +0200

    feat: add form request handling

--- src/components/shared/Modal.jsx
+++ src/components/shared/Modal.jsx
@@ -2,6 +2,10 @@ import { Button, Input } from "@material-tailwind/react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { useForm } from "react-hook-form";
 import { useEffect } from "react";
+import useRequest from "../../hooks/useRequest";
+import useFetch from "../../hooks/useFetch";
+import { useSnackbar } from "notistack";
+import { useNavigate } from "react-router";
 
 function Modal({ isOpen, setIsOpen, pokemon }) {
   const {
@@ -16,6 +20,12 @@ function Modal({ isOpen, setIsOpen, pokemon }) {
       exp: pokemon?.exp,
     },
   });
+  const { data: pokemonApi } = useFetch(
+    `http://localhost:3000/pokemons/${pokemon?.id}`,
+  );
+  const { send } = useRequest();
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (pokemon) {
@@ -27,7 +37,41 @@ function Modal({ isOpen, setIsOpen, pokemon }) {
     }
   }, [pokemon, reset]);
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = async (formData) => {
+    if (pokemonApi) {
+      const json = await send(
+        `http://localhost:3000/pokemons/${pokemon.id}`,
+        "PATCH",
+        {
+          height: Number(formData.height),
+          weight: Number(formData.weight),
+          exp: Number(formData.exp),
+        },
+      );
+      if (json) {
+        enqueueSnackbar(`Zmieniono atrybuty ${pokemon.name}`, {
+          variant: "success",
+        });
+        navigate("/home");
+      } else {
+        enqueueSnackbar(`Błąd edycji`, {
+          variant: "error",
+        });
+      }
+    } else {
+      const json = await send(
+        `http://localhost:3000/pokemons/${pokemon.id}`,
+        "POST",
+        {
+          id: String(pokemon.id),
+          height: Number(formData.height),
+          weight: Number(formData.weight),
+          exp: Number(formData.exp),
+        },
+      );
+      if (json) console.log(json);
+    }
+  };
 
   return (
     isOpen && (
