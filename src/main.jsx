25694a75166532522b39fa6933dcc88d20b948ed
Artur <arthal893@gmail.com>
2026-04-06 11:09:13 +0200

    chore: update favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "@material-tailwind/react";
-import { BrowserRouter, Routes, Route } from "react-router";
-import { SnackbarProvider } from "notistack";
 import "./index.css";
 import App from "./App.jsx";
-import Register from "./components/subpages/Register.jsx";
-import Login from "./components/subpages/Login.jsx";
+import Arena from "./components/subpages/Arena.jsx";
+import Edit from "./components/subpages/Edit.jsx";
 import Home from "./components/subpages/Home.jsx";
+import Login from "./components/subpages/Login.jsx";
+import PokemonDetails from "./components/subpages/PokemonDetails.jsx";
+import Ranking from "./components/subpages/Ranking.jsx";
+import Register from "./components/subpages/Register.jsx";
+import { ArenaProvider } from "./context/ArenaProvider.jsx";
 import { LoginProvider } from "./context/LoginProvider.jsx";
 import { PokemonProvider } from "./context/PokemonProvider.jsx";
-import { ArenaProvider } from "./context/ArenaProvider.jsx";
+import { ThemeProvider } from "@material-tailwind/react";
+import { SnackbarProvider } from "notistack";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { Navigate } from "react-router";
-import PokemonDetails from "./components/subpages/PokemonDetails.jsx";
-import Arena from "./components/subpages/Arena.jsx";
-import Ranking from "./components/subpages/Ranking.jsx";
-import Edit from "./components/subpages/Edit.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
